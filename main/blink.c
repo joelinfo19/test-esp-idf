c9f422385ccf85f354aaeeac3e8a8b2bf2505ba2
joel <joel_zerg_pro@outlook.com>
2022-02-09 23:25:01 -0500

    test001

--- main/blink.c
+++ main/blink.c
@@ -11,7 +11,8 @@
 #include "freertos/task.h"
 #include "driver/gpio.h"
 #include "sdkconfig.h"
-#include "../testable/Include/test.h"
+//#include "../testable/Include/test.h"
+#include "testone.h"
  //Can use project configuration menu (idf.py menuconfig) to choose the GPIO to blink,
    //or you can edit the following line and set a number here.
 
@@ -28,16 +29,25 @@ void app_main()
     gpio_pad_select_gpio(BLINK_GPIO);
    //  Set the GPIO as a push/pull output
     gpio_set_direction(BLINK_GPIO, GPIO_MODE_OUTPUT);
+    printf("no\n");
     while(1) {
         // Blink off (output low)
 	printf("Turning off the LED %i \n", test(0));
         gpio_set_level(BLINK_GPIO, 0);
         vTaskDelay(1000 / portTICK_PERIOD_MS);
+        printf("no\n");
+
       //   Blink on (output high)
 	printf("Turning on the LED %i \n",test(1));
         gpio_set_level(BLINK_GPIO, 1);
         vTaskDelay(1000 / portTICK_PERIOD_MS);
     }
+
+
+
+
+
+
 }
 
 /*
